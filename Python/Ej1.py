54fac20d676938a7877e3de6be95c25dc9df6371
Mauricio Martinez <68654041+mauriciomartinezm@users.noreply.github.com>
2024-05-24 00:35:21 -0500

    Add files via upload

---
+++ Python/Ej1.py
@@ -0,0 +1,14 @@
+def sumar(n1, n2):
+    resultado = n1 + n2
+    print("El resultado de la suma es: ", resultado)
+
+
+numero1 = float(input("Digite su primer numero: "))
+numero2 = float(input("Digite su primer numero: "))
+
+sumar(numero1, numero2)
+
+numero1 = float(input("Digite su primer numero: "))
+numero2 = float(input("Digite su primer numero: "))
+
+sumar(numero1, numero2)
---
+++ Python/ej2.py
@@ -0,0 +1,3 @@
+
+numero1 = int(input("Digite su primer numero: "))
+numero2 = int(input("Digite su segundo numero: "))
